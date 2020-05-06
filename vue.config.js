8d4811106bd87f2cafb56d476b06848c0fad34d6
KangTao <1421458857@qq.com>
2020-05-06 16:48:50 +0800

    development environment completed

---
+++ vue.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  lintOnSave: true,
+  devServer: {
+    // host: "127.0.0.1",
+    port: 3000,
+    // 这种形式只能跨单域,如果有多个资源服务器,需要配置多域,要用对象的形式
+    // proxy:"http://localhost:4000",
+    proxy: {
+      "/4000": {
+        target: "http://localhost:4000",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/4000": ""
+        }
+      }
+    },
+    open: true,
+    disableHostCheck: true
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'pages': resolve('src/pages'),
+        'components': resolve('src/components'),
+        'store': resolve('src/store'),
+        'http': resolve('src/http'),
+        'common': resolve('src/common'),
+        'util': resolve('src/util'),
+        'routes': resolve('src/routes'),
+        'router': resolve('src/router'),
+      }
+    },
+  }
+}
